8d3d490821be66cd6294345a5a9e94dfb4ac42e8
xiang940804 <xiang13140804@gmail.com>
2025-01-23 00:44:38 +0800

    美化login頁面，多了card來放註冊的畫面

--- client/crypto-client/src/pages/LoginPage.tsx
+++ client/crypto-client/src/pages/LoginPage.tsx
@@ -4,16 +4,6 @@ import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
-// 客製化css
-import '../css/LoginPage.css';
-import '../css/bootstrap.min.css';
-import '../css/style.css';
-
-// 客製化JS
-import '../js/jquery.min.js';
-import '../js/bootstrap.min.js';
-import '../js/main.js';
-
 // 客製化圖片
 import bgImage from '../images/bg.jpg';
 
@@ -38,59 +28,100 @@ function LoginPage() {
   return (
     <div className="img js-fullheight aaa" style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="mask">
-            <section className="ftco-section" >
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-md-6 text-center mb-5">
-                    <h2 className="heading-section">炒幣萬歲</h2>
-                  </div>
-                </div>
-                <div className="row justify-content-center">
-                  <div className="col-md-6 col-lg-4">
-                    <div className="login-wrap p-0">
-                      <h3 className="mb-4 text-center">有帳號嗎？</h3>
-                      <form action="#" className="signin-form">
-                        <div className="form-group">
-                          <input type="text" className="form-control" placeholder="Username" required />
-                        </div>
-                        <div className="form-group">
-                          <input id="password-field" type="password" className="form-control" placeholder="Password" required
-                          />
-                          {/* <span toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span> */}
-                        </div>
-                        <div className="form-group">
-                          <button type="submit" className="form-control btn btn-primary submit px-3">
-                            Sign In
-                          </button>
-                        </div>
-                        <div className="form-group d-md-flex">
-                          <div className="w-50">
-                            <label className="checkbox-wrap checkbox-primary">
-                              Remember Me
-                              <input type="checkbox" defaultChecked />
-                              <span className="checkmark"></span>
-                            </label>
+        <section className="ftco-section">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-md-6 text-center mb-5">
+                <h2 className="heading-section">炒幣萬歲</h2>
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <div className="login-wrap p-0 text-center">
+                  <h6 className="mb-0 pb-3"><span>登入 </span><span>註冊</span></h6>
+                  <input className="checkbox reg-log" type="checkbox" id="reg-log" name="reg-log" />
+                  <label htmlFor="reg-log"></label>
+                  <div className="card-3d-wrap">
+                    <div className="card-3d-wrapper">
+                      {/* 登入 Page */}
+                      <div className="card-front">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <h4 className="mb-4 pb-3" style={{ color: '#fff' }}>登入</h4>
+                            <form className="signin-form">
+                              <div className="form-group">
+                                <input type="text" className="form-control" placeholder="您的姓名" required />
+                              </div>
+                              <div className="form-group">
+                                <input type="password" className="form-control" placeholder="您的密碼" required />
+                              </div>
+                              <div className="form-group">
+                                <button type="submit" className="form-control btn btn-primary submit px-3">登入</button>
+                              </div>
+                              <div className="form-group d-md-flex">
+                                <div className="w-50">
+                                  <label className="checkbox-wrap checkbox-primary">
+                                    記住密碼
+                                    <input type="checkbox" defaultChecked />
+                                    <span className="checkmark"></span>
+                                  </label>
+                                </div>
+                                <div className="w-50 text-md-right">
+                                  <a href="#" style={{ color: '#fff' }}>忘記密碼?</a>
+                                </div>
+                              </div>
+                            </form>
                           </div>
-                          <div className="w-50 text-md-right">
-                            <a href="#" style={{ color: '#fff' }}>
-                              Forgot Password
-                            </a>
+                        </div>
+                        <p className="w-100 text-center mt-4">&mdash; Or Sign In With &mdash;</p>
+                        <div className="social d-flex text-center">
+                          <a onClick={handleGoogleSignIn} className="px-2 py-2 mr-md-1 rounded btn btn-google">
+                            <span className="fa fa-google mr-2"></span> Google
+                          </a>
+                          <a href="#" className="px-2 py-2 ml-md-1 rounded">
+                            <span className="ion-logo-twitter mr-2"></span> Twitter
+                          </a>
+                        </div>
+                      </div>
+                      {/* 註冊 Page */}
+                      <div className="card-back">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <h4 className="mb-4 pb-3" style={{ color: '#fff' }}>註冊</h4>
+                            <form className="signup-form">
+                              <div className="form-group">
+                                <input type="text" className="form-control" placeholder="您的姓名" required />
+                              </div>
+                              <div className="form-group">
+                                <input type="email" className="form-control" placeholder="您的電子郵件" required />
+                              </div>
+                              <div className="form-group">
+                                <input type="password" className="form-control" placeholder="您的密碼" required />
+                              </div>
+                              <div className="form-group">
+                                <button type="submit" className="form-control btn btn-primary submit px-3">註冊</button>
+                              </div>
+                            </form>
                           </div>
                         </div>
-                      </form>
-                      <p className="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-                      <div className="social d-flex text-center">
-                        <a onClick={handleGoogleSignIn} className="px-2 py-2 mr-md-1 rounded btn btn-google">
-                          <span className="fa fa-google mr-2"></span> Google
-                        </a>
-                        <a href="#" className="px-2 py-2 ml-md-1 rounded"><span className="ion-logo-twitter mr-2"></span> Twitter</a>
+                        <p className="w-100 text-center mt-4">&mdash; Or Sign In With &mdash;</p>
+                        <div className="social d-flex text-center">
+                          <a onClick={handleGoogleSignIn} className="px-2 py-2 mr-md-1 rounded btn btn-google">
+                            <span className="fa fa-google mr-2"></span> Google
+                          </a>
+                          <a href="#" className="px-2 py-2 ml-md-1 rounded">
+                            <span className="ion-logo-twitter mr-2"></span> Twitter
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </section>
-        </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
